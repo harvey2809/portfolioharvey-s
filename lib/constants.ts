19953cbd26335a39c3e205004c97c6ac43f4e705
harvey2809 <hardeepbhatti2809@gmail.com>
2025-08-14 17:42:46 -0400

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "Hardeep Bhatti - Environmental and Land Use planner";
 export const SITE_DESCRIPTION =
-  "Portfolio of a Full Stack Developer with 5 years of experience";
+  "Portfolio of a Environmental planner";
 
 export const CATEGORIES = [
   "All",
@@ -8,28 +8,28 @@ export const CATEGORIES = [
 
 export const PROJECTS = [
   {
-    title: "E-commerce Platform",
+    title: "Natural Neritage Evaluation",
     description:
-      "A full-stack e-commerce solution with real-time inventory management",
-    category: "Full Stack",
-    technologies: ["React", "Node.js", "MongoDB", "WebSocket"],
-    backgroundColor: "bg-pink-50",
-    darkBackgroundColor: "dark:bg-pink-900",
+      "A Detailed Natural Heritage Evaluation Report on a Development Area in ORCM area ",
+    category: "All",
+    technologies: ["Evaluation report", "Policy complience", "Biodiversity Research", " Report writing"],
+    backgroundColor: "bg-emerald-50",
+    darkBackgroundColor: "dark:bg-emerald-900",
   },
   {
-    title: "Mobile Banking App",
+    title: "Ecological Compensation Policy",
     description:
-      "Secure mobile banking application with biometric authentication",
-    category: "Mobile",
-    technologies: ["React Native", "TypeScript", "Redux"],
-    backgroundColor: "bg-purple-50",
-    darkBackgroundColor: "dark:bg-purple-900",
+      "Detailed Policy And gudelineline framework for Municipality of Port Hope",
+    category: "All",
+    technologies: ["Policy Writing", "Watershed planning", "Spatial analysis"],
+    backgroundColor: "bg-grey-50",
+    darkBackgroundColor: "dark:bg-grey-900",
   },
   {
-    title: "Analytics Dashboard",
-    description: "Real-time analytics dashboard with interactive charts",
-    category: "Frontend",
-    technologies: ["Next.js", "D3.js", "TailwindCSS"],
+    title: "Provincial Policy Design",
+    description: "A Proposed provincial park design in Goderich, ON after thorough analysis of the Municipal area.",
+    category: "All",
+    technologies: ["ArcGIS", "Urban Design", "Parks Planning", "Spatial Data interpretation"],
     backgroundColor: "bg-green-50",
     darkBackgroundColor: "dark:bg-green-900",
   },
@@ -42,60 +42,20 @@ export const PROJECTS = [
     darkBackgroundColor: "dark:bg-yellow-900",
   },
   {
-    title: "Social Media Scheduler",
-    description:
-      "A tool for scheduling and analyzing social media posts across multiple platforms",
-    category: "Full Stack",
-    technologies: ["React", "Express", "PostgreSQL", "Bull"],
+    title: "More projects Posting Soon",
+    description: " ",
+    category: "All",
+    technologies: [" ", " ", " ", " "],
     backgroundColor: "bg-blue-50",
     darkBackgroundColor: "dark:bg-blue-900",
   },
   {
-    title: "IoT Device Management Platform",
-    description: "A platform for managing and monitoring IoT devices at scale",
-    category: "Full Stack",
-    technologies: ["Vue.js", "Node.js", "MQTT", "InfluxDB"],
+    title: "More Projects Comming Soon",
+    description: " ",
+    category: "All",
+    technologies: [" ", " ", " ", " "],
     backgroundColor: "bg-indigo-50",
     darkBackgroundColor: "dark:bg-indigo-900",
-  },
-];
-
-export const BLOG_POSTS = [
-  {
-    title: "The Future of Web Development: What to Expect in 2024",
-    excerpt:
-      "Explore the upcoming trends and technologies that will shape the web development landscape in the coming year.",
-    date: "2023-12-01",
-    tags: ["Web Development", "Trends", "Technology"],
-    slug: "some-ththt-ththt",
-    id: "1323",
-  },
-  {
-    title: "Optimizing React Applications for Performance",
-    excerpt:
-      "Learn advanced techniques to improve the performance of your React applications and deliver a smoother user experience.",
-    date: "2023-11-15",
-    tags: ["React", "Performance", "Optimization"],
-    slug: "some-ththt-ththt",
-    id: "1323",
-  },
-  {
-    title: "Building Scalable Backend Systems with Node.js",
-    excerpt:
-      "Discover best practices and architectural patterns for creating robust and scalable backend systems using Node.js.",
-    date: "2023-10-30",
-    tags: ["Node.js", "Backend", "Scalability"],
-    slug: "some-ththt-ththt",
-    id: "1323",
-  },
-  {
-    title: "The Impact of AI on Modern Web Development",
-    excerpt:
-      "Explore how artificial intelligence is revolutionizing web development and what it means for developers.",
-    date: "2023-10-12",
-    tags: ["AI", "Web Development", "Innovation"],
-    slug: "some-ththt-ththt",
-    id: "1323",
   },
 ];
 
@@ -113,40 +73,6 @@ export const SKILLS = [
   "AWS",
   "CI/CD",
 ];
-
-export const WORK_EXPERIENCE = [
-  {
-    title: "Software Development Engineer 2",
-    company: "Fisdom",
-    period: "Oct 2024-Present",
-    description:
-      "Led development of critical app modules, implemented real-time data handling with WebSockets, and optimized performance through code splitting and lazy loading, resulting in 25% fewer support tickets, 40% faster data processing, and 35% reduced load times.",
-  },
-  {
-    title: "Associate Software Developer",
-    company: "Bluespacelabs",
-    period: "March 2021- May 2022",
-    description:
-      "Led development of 'Aquila' e-commerce app (4.5-star rating, 100,000+ downloads) and 'Bluwyare' SASS messaging platform using React Native/ReactJS, while implementing CI/CD pipelines that reduced deployment time by 40%.",
-
-  },
-  {
-    title: "Web Developer",
-    company: "IIH Global",
-    period: "July 2020 - Feb 2021",
-    description:
-      "Spearheaded end-to-end development of a ReactJS pharmacy management application with inventory tracking and prescription features, while refactoring legacy code with Redux-Saga and Material UI for 50% improved maintainability.",
-  },
-  {
-    title: "Freelance Web Developer",
-    company: "G Media Solution",
-    period: "March 2019 - April 2020",
-    description:
-        "Enhanced an e-commerce platform using ESLint and Prettier standards for code quality, while developing a 30+ screen React Native matrimony app with Firebase integration and Redux for efficient state management.",
-
-  },
-];
-
 export const CONTACT_INFO = {
   email: "Hardeepbhatti2809@gmail.com",
   phone: "+1 437(881)-2809",
@@ -156,6 +82,5 @@ export const CONTACT_INFO = {
 export const NAV_LINKS = [
   { href: "/about", label: "About" },
   { href: "/projects", label: "Projects" },
-  { href: "/blog", label: "Blog" },
   { href: "/contact", label: "Contact" },
 ];
