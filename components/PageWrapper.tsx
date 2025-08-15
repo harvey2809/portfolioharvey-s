a39f5233a49f6b8ce7622642744fd7ad22322492
harvey2809 <hardeepbhatti2809@gmail.com>
2025-08-14 22:12:36 -0400

    Update PageWrapper.tsx

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,1 +1,14 @@
+import React from "react";
+
+export default function PageWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-black/40 text-white p-6 rounded-xl shadow-lg backdrop-blur-md">
+      {children}
+    </div>
+  );
+}
 
