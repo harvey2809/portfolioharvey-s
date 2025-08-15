18c611387095505610b5334762344dd35019e79f
harvey2809 <hardeepbhatti2809@gmail.com>
2025-08-14 21:29:16 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -10,7 +10,7 @@ export function Header() {
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="hidden font-bold sm:inline-block">
-              Aditya Kumar
+              Harvey
             </span>
           </Link>
           <nav className="flex items-center space-x-6 text-sm font-medium">
